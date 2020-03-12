ba97904c586327fd1ca5c7d1ba425b2abf4070a7
Derek H <dhass421@gmail.com>
2020-03-12 00:52:58 -0400

    allowed update of status, small fixes

--- js/backendconn.js
+++ js/backendconn.js
@@ -11,14 +11,15 @@ $("#loginForm").submit(function(event)
         type: "POST",
         url: "http://localhost:5000/api/users/login",
         data: objectDataString,
-        error: function (e) {
+        error: function (e) {          
           console.log(e);
          },
         dataType:"json",
         contentType: "application/json",
         success: function(result)
         {
-          //add method to go to create request page
+          console.log(result);
+          return window.location.href = "requests.html";
         },
 
     });
@@ -51,6 +52,7 @@ $("#signupForm").submit(function(event)
         contentType: "application/json",
         success: function(result)
         {
+          console.log(result);
           $("#alert").load("../include/alertsuccess.html", function()
           {
             $('#signupForm').each(function(){
@@ -69,7 +71,7 @@ $("#requestForm").submit(function(event)
     var objectDataString = JSON.stringify({
       "message": document.getElementById('message').value,
       "category": document.getElementById('category').value,
-      "approvalStatus": false
+      "approvalStatus": "Pending"
     });
 
     event.preventDefault();
@@ -109,22 +111,81 @@ function loadRequests() {
     success: function (response) {
 
       $.each(response.data, function(i, item) {
+
+        if(item.approvalStatus == "Pending") {
+          var $dropdown = $('<select>').toggleClass("form-control approvalStatus").append(
+            new Option("Pending", "Pending"),
+            new Option("Approved", "Approved"),
+            new Option("Denied", "Denied"),
+          );
+        } else {
+          var $dropdown = item.approvalStatus;
+        }
+
         console.log(i);
           var $tr = $('#tabledata').append(
             $('<tr>').append(
             $('<td>').text(item._id),
             $('<td>').text(item.message),
             $('<td>').text(item.category),
-            $('<td>').append(
-              $('<select>').toggleClass("form-control approvalStatus").append(
-                new Option("Pending Approval", null),
-                new Option("Approved", true),
-                new Option("Denied", false),
-              ),
-            )
+            $('<td>').append($dropdown)
           )
         ); 
       })   
     }
   })
+}
+
+function updateRequests() {
+
+
+
+  $('#requesttable').each(function(){
+    $(this).find('tr').each(function(){
+      
+      row = [];
+
+      row.push($(this).find("td:eq(0)").text());
+      row.push($(this).find("td:eq(1)").text());
+      row.push($(this).find("td:eq(2)").text());
+      if($(this).find("td:eq(3)").text() =="Approved" || $(this).find("td:eq(3)").text() =="Denied") {
+        row.push($(this).find("td:eq(3)").text());
+      } else {
+        console.log($(this).find("td:eq(3) option:selected").text());
+        row.push($(this).find("td:eq(3) option:selected").text());
+      }
+      postUpdateRequests(row);
+      row = [];
+    })
+})
+}
+
+function postUpdateRequests(data) {
+  
+  console.log("http://localhost:5000/api/requests/"+data[0]);
+
+  var objectDataString = JSON.stringify({
+    //"_id": data[0],
+    "message": data[1],
+    "category": data[2],
+    "approvalStatus": data[3]
+  });
+  
+  $.ajax({
+    type: "PUT",
+    crossDomain: true,
+    url: "http://localhost:5000/api/requests/"+data[0],
+    data: objectDataString ,
+    error: function (e) {
+      console.log(e);
+     },
+    dataType:"json",
+    contentType: "application/json",
+    success: function(result)
+    {
+      console.log(result);
+      return window.location.href = "requests.html";
+    },
+
+});
 }
